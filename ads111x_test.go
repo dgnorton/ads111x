2abf57791fdf31d3d0c9af8e073837a346313142
David Norton <dgnorton@gmail.com>
2017-01-09 23:11:32 -0500

    delete unit test cruft

--- ads111x_test.go
+++ ads111x_test.go
@@ -7,31 +7,6 @@ import (
 	"testing"
 )
 
-//func msb(n uint16) uint8 { return uint8((n & 0xFF00) >> 8) }
-//func lsb(n uint16) uint8 { return uint8(n & 0x00FF) }
-
-//func Test_toBytes(t *testing.T) {
-//	b, err := toBytes(uint16(0x8583))
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	if b[1] != 0x85 || b[0] != 0x83 {
-//		fmt.Printf("0x%x\n", bytesToUint16(b))
-//		t.Fatalf("exp = 0x%x, got = 0x%x%x\n", DefaultConfig, b[1], b[0])
-//	}
-//}
-//
-//func Test_toUint16(t *testing.T) {
-//	b := []byte{0x83, 0x85}
-//	v, err := toUint16(b)
-//	if err != nil {
-//		t.Fatal(err)
-//	} else if v != DefaultConfig {
-//		t.Fatalf("exp = 0x%x, got = 0x%x", DefaultConfig, v)
-//	}
-//}
-
 func Test_WriteReg(t *testing.T) {
 	if err := newTestADC().WriteReg(ConfigReg, DefaultConfig); err != nil {
 		t.Fatal(err)
@@ -49,6 +24,7 @@ func Test_SetMode(t *testing.T) {
 	i2c := adc.i2c.(*mockI2C)
 	i2c.expReg = ConfigReg
 	copy(i2c.expDat, []byte{0x84, 0x83})
+
 	if err := adc.SetMode(Continuous); err != nil {
 		t.Fatal(err)
 	}
